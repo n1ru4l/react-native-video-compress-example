150d66bea65b9939e6131c5fee2c5f3e273c9f8b
Laurin Quast <laurinquast@googlemail.com>
2018-02-26 20:05:50 +0100

    Add react-native-camera

---
+++ ios/ReactNativeVideoCompressExample/Services/VideoCompressService.h
@@ -0,0 +1,13 @@
+//
+//  VideoCompressService.h
+//  ReactNativeVideoCompressExample
+//
+//  Created by Laurin Quast on 26.02.18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#ifndef VideoCompressService_h
+#define VideoCompressService_h
+
+
+#endif /* VideoCompressService_h */
---
+++ ios/ReactNativeVideoCompressExample/Services/VideoCompressService.m
@@ -0,0 +1,9 @@
+//
+//  VideoCompressService.m
+//  ReactNativeVideoCompressExample
+//
+//  Created by Laurin Quast on 26.02.18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
